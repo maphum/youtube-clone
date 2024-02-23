89e6b03c1fa55a2d0324d1112a74169d0fb1f142
LENN <nam.lenguyenkhanh@elca.vn>
2024-02-23 15:50:40 +0700

    Refactor Code

--- utils/getMetaData.js
+++ utils/getMetaData.js
@@ -1,45 +1,32 @@
 import { REACT_APP_API_KEY } from "../constants/ApiKey";
+
+// Utility function to fetch logo URL
+const fetchLogo = async (channelId) => {
+    const url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet&part=statistics&id=${channelId}&key=${REACT_APP_API_KEY}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.items[0].snippet.thumbnails.default.url;
+};
+
+// Enhance list with logo URLs
 export const listWithLogos = async (result) => {
-    const fetchLogo = async (channel_id) => {
-        var URL = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet&part=statistics&id=${channel_id}&key=${REACT_APP_API_KEY}`;
-        const res = await fetch(URL, {
-            method: "GET", 
-            headers: {},
-        });
-        const logodata = await res.json();
-        let logo = logodata.items[0].snippet.thumbnails.default.url;
-        return logo;
-    };
-    let v = result?.items;
-    v = v.map(async item => {
-        item.logo = await fetchLogo(item.snippet.channelId)
-        return item
-    })
+    const videos = result?.items ?? [];
+    const enhancedVideos = videos.map(async (item) => {
+        const logo = await fetchLogo(item.snippet.channelId);
+        return { ...item, logo };
+    });
+    return Promise.all(enhancedVideos);
+};
 
-    let finalList = await Promise.all(v).then((res) => {
-        
-        return res
-    })
-    return finalList
-}
+// Enhance video list with statistics
 export const listWithStatistics = async (videoList) => {
+    const videosId = videoList.map(item => item?.id?.videoId).filter(id => id).join(',');
+    const url = `https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${videosId}&key=${REACT_APP_API_KEY}`;
+    const response = await fetch(url);
+    const data = await response.json();
 
-    let initValue = ''
-    const videosId = videoList.reduce(
-        ((accumulator, currentValue) => {
-            console.log(currentValue)
-            if (accumulator)
-                return accumulator + ',' + currentValue?.id?.videoId
-            else return currentValue?.id?.videoId
-        }),
-        initValue
-    );
-    let r = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${videosId}&key=${REACT_APP_API_KEY}`).then(res => res.json())
-    
-    videoList = videoList.map((item, i) => {
-        item.statistics = r.items[i].statistics
-        return item
-    })
-    
-    return videoList
-}
+    return videoList.map((item, i) => ({
+        ...item,
+        statistics: data.items[i]?.statistics,
+    }));
+};
